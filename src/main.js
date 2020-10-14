c646cefb40c730ffb937d1619ea7d75c131b00bc
wangkaiwd <1162486176@qq.com>
2020-10-14 11:22:53 +0800

    feat: deploy project to github page

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import Vue from 'vue';
 import App from './App.vue';
 import '@/assets/style/reset.scss';
-import GoLazyLoad from '@/components/lazy-load';
+import LazyLoad from '@/components/lazy-load';
 
-Vue.use(GoLazyLoad, {
+Vue.use(LazyLoad, {
   preload: 1.3,
   error: require('@/assets/imgs/error.png'),
   loading: require('@/assets/imgs/loading.png')
